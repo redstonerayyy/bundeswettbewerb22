eeca2ce5462e5ed6aaf878944a15ca5a5f7d4b86
Redstonerayy <redstone.rayy@gmail.com>
2022-09-01 14:09:01 +0200

    next

--- j1/j1.py
+++ j1/j1.py
@@ -5,13 +5,17 @@ vocals = "aeiou"
 def getPart(word : string) -> string:
 	word = word.lower()
 	part = ""
+	waitonvocal = ""
 	vokalgroups = 0
 	lastwasvocal = False
 	for i in word[::-1]:
 		if i in vocals:
-			
+			if vocalgroups < 2:
+
+			lastwasvocal = True
+		else:
 
 
 words = ["Baum", "Traum", "singen", "klingen"]
 for i in words:
-	part = i.	
+	part = i.
